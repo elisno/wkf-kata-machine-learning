d42775770df2e586d6d5599a8a0029d777ec7d0b
Elías Snorrason <els11@hi.is>
2020-03-21 01:30:20 +0000

    Move data utilities into a seperate module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wkf-kata-machine-learning',
+    version='0.0.1',
+    install_requires=[
+        'pytest'
+    ],
+    packages=find_packages()
+)
